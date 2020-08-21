bc0d7584b6c05e651aab4b62da8a9513067e3ea8
David Salazar <davidjsalazarmoreno@gmail.com>
2020-08-20 21:43:38 -0500

    setups database

--- server.ts
+++ server.ts
@@ -1,5 +1,41 @@
-import { Application, send } from "./deps.ts";
-import router from "./router.ts";
+import { Application, Database } from "./deps.ts";
+import { Character } from "./models/character.ts";
+import { Film } from "./models/film.ts";
+import { Planet } from "./models/planet.ts";
+import { Specie } from "./models/specie.ts";
+import { Starship } from "./models/starship.ts";
+import { Vehicle } from "./models/vehicle.ts";
+import { router } from "./router.ts";
+
+const db = new Database("sqlite3", {
+  filepath: "./database.sqlite",
+});
+
+db.link([Character, Film, Planet, Specie, Starship, Vehicle]);
+
+Character.hasMany(Film);
+Character.hasMany(Specie);
+Character.hasMany(Starship);
+Character.hasMany(Vehicle);
+
+Film.hasMany(Character);
+Film.hasMany(Planet);
+Film.hasMany(Starship);
+Film.hasMany(Vehicle);
+
+Starship.hasMany(Film);
+Starship.hasMany(Character);
+
+Vehicle.hasMany(Film);
+Vehicle.hasMany(Character);
+
+Specie.hasMany(Film);
+Specie.hasMany(Character);
+
+Planet.hasMany(Film);
+Planet.hasMany(Character);
+
+await db.sync({ drop: true });
 
 const app = new Application();
 
