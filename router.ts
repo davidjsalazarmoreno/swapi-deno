010a6e978d1b0fc43259df252670970e8d9595de
David Salazar <davidjsalazarmoreno@gmail.com>
2020-08-20 21:43:09 -0500

    removes default export

--- router.ts
+++ router.ts
@@ -1,9 +1,7 @@
 import { Router } from "./deps.ts";
 
-const router = new Router();
+export const router = new Router();
 
 router.get("/", () => {
   console.log("Hi");
 });
-
-export default router;
