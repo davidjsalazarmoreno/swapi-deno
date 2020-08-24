c29edd4f677991b34b9ee083c93801f6e0aabf71
David Salazar <davidjsalazarmoreno@gmail.com>
2020-08-23 19:44:00 -0500

    removes denodb as dependency

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ export {
   Application,
   send,
 } from "https://deno.land/x/oak@v6.0.1/mod.ts";
-export { DataTypes, Database, Model } from 'https://deno.land/x/denodb@v1.0.7/mod.ts';
+export { DB } from "https://deno.land/x/sqlite@v2.3.0/mod.ts";
