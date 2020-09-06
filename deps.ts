4dbb1970e0df0f347490b55004ba3406f4e27b1d
David Salazar <davidjsalazarmoreno@gmail.com>
2020-09-06 14:54:35 -0500

    adds case as dependency

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
+export { titleCase } from "https://deno.land/x/case@v2.1.0/mod.ts";
 export {
-  Router,
   Application,
+  Router,
   send,
 } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v2.3.0/mod.ts";
