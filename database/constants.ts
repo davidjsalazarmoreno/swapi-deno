9fa06e220b1475021e1f9c9cc3cb9bffbfea4e10
David Salazar <davidjsalazarmoreno@gmail.com>
2020-09-06 15:15:05 -0500

    adds db constants module

---
+++ database/constants.ts
@@ -0,0 +1,9 @@
+export const tableNames: { [key: string]: string } = {
+  film: "films",
+  people: "people",
+  planet: "planets",
+  species: "species",
+  starship: "starships",
+  transport: "transports",
+  vehicle: "vehicles",
+};
