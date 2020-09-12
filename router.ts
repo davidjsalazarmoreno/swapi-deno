7704267de3ce0b335f35a72648c8ae94cbbd26e7
David Salazar <davidjsalazarmoreno@gmail.com>
2020-09-11 22:36:27 -0500

    adds people test route

--- router.ts
+++ router.ts
@@ -1,7 +1,13 @@
-import { Router } from "./deps.ts";
+import { Router, RouterContext } from "./deps.ts";
+import { People } from "./models/people.ts";
 
 export const router = new Router();
 
 router.get("/", () => {
   console.log("Hi");
+});
+
+router.get("/api/people", async (ctx: RouterContext) => {
+  const people = [...await new People().getAll().asObjects()];
+  ctx.response.body = people;
 });
