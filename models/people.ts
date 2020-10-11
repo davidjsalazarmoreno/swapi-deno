25363783604e3df365a73db24b358727be05fbdf
David Salazar <davidjsalazarmoreno@gmail.com>
2020-10-10 19:01:25 -0500

    updates relations

--- models/people.ts
+++ models/people.ts
@@ -5,24 +5,52 @@ export class People {
 
   public getAll() {
     return db.query(`/* SQL */
-      SELECT 
-        id, 
-        birth_year,
-        eye_color,
-        gender,
-        hair_color,
-        height,
-        homeworld,
-        mass,
-        name,
-        skin_color,
-        url,
-        edited,
-        created,
-        film_id
+     SELECT DISTINCT
+        pl.id, 
+        pl.birth_year,
+        pl.eye_color,
+        pl.gender,
+        pl.hair_color,
+        pl.height,
+        pl.homeworld,
+        pl.mass,
+        pl.name,
+        pl.skin_color,
+        pl.url,
+        pl.edited,
+        pl.created,
+        GROUP_CONCAT(
+          DISTINCT fc.filmId 
+        ) films,
+        GROUP_CONCAT(
+          DISTINCT sp.specieId 
+        ) species,
+        GROUP_CONCAT(
+          DISTINCT stpl.starshipId 
+        ) starthips
       FROM 
-        people
-      ;
+        people pl
+      LEFT JOIN 
+        filmCharacters fc
+      ON 
+        fc.characterId = pl.id
+      LEFT JOIN 
+        speciesPeople sp
+      ON 
+        sp.characterId = pl.id
+      LEFT JOIN 
+        vehicles
+      ON
+        sp.characterId = pl.id
+      LEFT JOIN
+        starshipPilots stpl
+      ON
+        pl.id = stpl.characterId
+      GROUP BY
+        pl.id
+      ORDER BY
+        pl.id
+      ; 
     `);
   }
 }
