e5082c32468fe7d59376a8f7239796764ade34ab
David Salazar <davidjsalazarmoreno@gmail.com>
2021-02-01 19:27:19 -0500

    adds error handling

--- client/src/routes/home/Home.svelte
+++ client/src/routes/home/Home.svelte
@@ -11,7 +11,8 @@
     const api = "http://localhost:8000";
     return fetch(`${api}${uri}`)
       .then((r) => r.json())
-      .then((data) => JSON.stringify(data, null, 4));
+      .then((data) => JSON.stringify(data, null, 4))
+      .catch(() => "No data for the requested resource");
   }
 
   async function onResourceRequest(uri) {
