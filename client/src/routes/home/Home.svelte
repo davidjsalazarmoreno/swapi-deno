2e39de3a8b8188ea9d28a52b1008ca8c09d0d93b
David Salazar <davidjsalazarmoreno@gmail.com>
2021-02-05 18:47:58 -0500

    removes padding and limits code block height

--- client/src/routes/home/Home.svelte
+++ client/src/routes/home/Home.svelte
@@ -89,10 +89,10 @@
   >
     <h2 class="text-5xl font-bold m-8">Result:</h2>
     <div
-      class="overflow-y-scroll w-11/12 bg-gray-900 border-black border-2 p-7 rounded-xl"
+      class="overflow-hidden w-11/12 bg-gray-900 border-black border-2 p-7 rounded-xl"
     >
       <pre
-        class="bg-gray-100 text-gray-600 p-4 rounded-xl overflow-scroll">
+        class="bg-gray-100 text-gray-600 p-4 rounded-xl overflow-scroll sm:max-h-96">
         <code class="json ">
           {json}
         </code>
