02683ec8bd528b59f149685f329e04c5bd7eaffd
David Salazar <davidjsalazarmoreno@gmail.com>
2021-02-05 21:52:05 -0500

    removes about

--- client/src/routes/home/Home.svelte
+++ client/src/routes/home/Home.svelte
@@ -38,11 +38,6 @@
           Article
         </a>
       </li>
-      <li>
-        <a class="hover:text-gray-200 transition no-underline" href="#">
-          About
-        </a>
-      </li>
     </ol>
   </nav>
 </header>
@@ -140,25 +135,29 @@
 </section>
 <!-- ./Three questions -->
 
-<footer class="p-5 bg-primary-swapi border-t-2 border-gray-900">
-  <p>
+<footer
+  class="bg-primary-swapi border-t-2 border-gray-900 p-5 flex justify-between"
+>
+  <p class="flex-grow">
     Created by David Salazar inspired on
     <a href="https://swapi.dev/"> https://swapi.dev/ </a>
   </p>
 
   <!-- Place this tag where you want the button to render. -->
-  <!-- <a
-      class="github-button"
-      href="https://github.com/davidjsalazarmoreno"
-      aria-label="Follow @davidjsalazarmoreno on GitHub">
-      Follow @davidjsalazarmoreno
-    </a>
+  <a
+    class="github-button"
+    href="https://github.com/davidjsalazarmoreno"
+    aria-label="Follow @davidjsalazarmoreno on GitHub"
+  >
+    Follow @davidjsalazarmoreno
+  </a>
 
-    <a
-      href="https://twitter.com/davidjsmoreno?ref_src=twsrc%5Etfw"
-      class="twitter-follow-button"
-      data-show-count="false">Follow @davidjsmoreno</a
-    > -->
+  <a
+    href="https://twitter.com/davidjsmoreno?ref_src=twsrc%5Etfw"
+    class="twitter-follow-button"
+    data-show-count="false"
+    >Follow @davidjsmoreno
+  </a>
 </footer>
 
 <style>
